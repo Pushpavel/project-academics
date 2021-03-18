9803f16c9cb9f5c61f74cd291e091a4dfec986e6
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-03-18 19:14:24 +0530

    bug: removing MDCDataTable in "material-components-web" as it has bug (it errors)

--- src/app/mdc-helper/mdc-table/mdc-table.component.ts
+++ src/app/mdc-helper/mdc-table/mdc-table.component.ts
@@ -1,7 +1,6 @@
-import {AfterViewInit, Component, ElementRef, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {MDCDataTable} from '@material/data-table';
 
 /**
  * this implementation does not sync rowDataChange with inputSource
@@ -15,13 +14,13 @@ import {MDCDataTable} from '@material/data-table';
     class: 'mdc-data-table'
   }
 })
-export class MdcTableComponent<T> implements AfterViewInit {
+export class MdcTableComponent<T extends Record<string, any>> {
 
   @Input() inputSource: Observable<readonly Readonly<T>[]> = of([]);
 
   @Input('columns') columnSettings: ColumnSetting<T>[] = [];
   @Input() calculateRow?: (v: Readonly<T>) => Readonly<T>;
-  @Output() rowDataChange?: EventEmitter<{ index: number, key: string, changed_row: Readonly<T> }>;
+  @Output() cellDataChange = new EventEmitter<CellDataChangeEvent<T>>();
 
   get _dataSource() {
     return this.inputSource?.pipe(
@@ -34,25 +33,14 @@ export class MdcTableComponent<T> implements AfterViewInit {
   };
 
 
-  constructor(private elementRef: ElementRef) {
-  }
-
-  ngAfterViewInit() {
-    // After ngOnInit, the `CdkTable` has created and inserted the table sections (thead, tbody,
-    // tfoot). MDC requires the `mdc-data-table__content` class to be added to the body.
-    const tbody = this.elementRef.nativeElement.querySelector('tbody');
-    tbody.classList.add('mdc-data-table__content');
-
-    MDCDataTable.attachTo(this.elementRef.nativeElement);
-  }
-
-
 }
 
 export type ColumnSetting<T extends Record<string, any> = any> = {
-  key: Extract<keyof T, string>,
+  key: keyof T,
   label: string,
   editable?: boolean,
   sortable?: boolean// NOT YET IMPLEMENTED
   flex?: number,
 }
+
+export type CellDataChangeEvent<T extends Record<string, any>> = { index: number, key: keyof T, changed_row: Readonly<T> };
