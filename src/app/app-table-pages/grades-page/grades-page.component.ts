fa54316fdd71d317b3238234b2759dbb55455d62
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-03-28 00:50:19 +0530

    grades-page.component basic data flow setup

--- src/app/app-table-pages/grades-page/grades-page.component.ts
+++ src/app/app-table-pages/grades-page/grades-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {map, shareReplay, switchMap} from 'rxjs/operators';
+import {GRADES_COLUMN_SETTINGS} from '@lib/constants/column-settings.constants';
+import {DocumentService} from '@service/document.service';
+import {ActivatedRoute} from '@angular/router';
+import {GradeEntry} from '@lib/models/grading.model';
 
 @Component({
   selector: 'app-grades-page',
   templateUrl: './grades-page.component.html',
   styleUrls: ['./grades-page.component.scss']
 })
-export class GradesPageComponent implements OnInit {
+export class GradesPageComponent {
 
-  constructor() { }
+  courseCode = this.route.paramMap.pipe(
+    map(params => params.get('course_code') ?? 'Error'),    // Todo: Handle if course_code is null
+    shareReplay(1),
+  );
 
-  ngOnInit(): void {
+  documentStat = this.courseCode.pipe(
+    // Todo: query with UserService about the access level or type of user
+    switchMap(courseCode => this.documentService.getStat(courseCode, 'GRADES', false)),
+    shareReplay(1),
+  );
+
+  gradeEntries = this.documentStat.pipe(
+    switchMap(doc =>
+      this.documentService.getEntries<GradeEntry>(doc.courseCode, 'GRADES', false)
+    ),
+  );
+
+  columns = GRADES_COLUMN_SETTINGS;
+
+  constructor(private documentService: DocumentService, private route: ActivatedRoute) {
   }
+
 
 }
--- src/lib/constants/column-settings.constants.ts
+++ src/lib/constants/column-settings.constants.ts
@@ -1,6 +1,7 @@
 import {ColumnSetting} from '../../app/mdc-helper/mdc-table/mdc-table.component';
 import {AttendanceEntry} from '@lib/models/attendance.model';
 import {MarklistEntry} from '@lib/models/marklist.model';
+import {GradeEntry} from '@lib/models/grading.model';
 
 export const ATTENDANCE_COLUMN_SETTINGS: ColumnSetting<AttendanceEntry>[] = [
   {key: 'rollNo', label: 'Roll No'},
@@ -13,4 +14,15 @@ export const MARKLIST_COLUMN_SETTINGS: ColumnSetting<MarklistEntry>[] = [
   {key: 'name', label: 'Name'},
   {key: 'rollNo', label: 'Roll No'},
   {key: 'mark', label: 'Mark'},
+];
+
+export const GRADES_COLUMN_SETTINGS: ColumnSetting<GradeEntry>[] = [
+  {key: 'rollNo', label: 'Roll No'},
+  {key: 'name', label: 'Name'},
+  {key: 'CT1', label: 'CT1'},
+  {key: 'CT2', label: 'CT2'},
+  {key: 'ASSIGNMENT', label: 'ASSIGNMENT'},
+  {key: 'END_SEM', label: 'END SEMESTER'},
+  {key: 'TOTAL', label: 'TOTAL'},
+  {key: 'GRADE', label: 'GRADE'},
 ];
