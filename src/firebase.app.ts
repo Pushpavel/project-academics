91117146700f5588aa42c6155afdc242c92edb1e
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-03-29 15:08:11 +0530

    firebase emulators setup

--- src/firebase.app.ts
+++ src/firebase.app.ts
@@ -10,3 +10,7 @@ export const auth = app.auth();
 export const firestore = app.firestore();
 // export const cloudFunction = app.functions('asia-south1');
 
+if (!environment.production) {
+  auth.useEmulator('http://localhost:9099');
+  firestore.useEmulator('localhost', 8080);
+}
