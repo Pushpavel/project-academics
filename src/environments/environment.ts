c7204aa7be1cff5f26108ffdd65d90765b9f9e8a
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-04-12 15:57:42 +0530

    auth emulator updated, testFaculty user created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,6 @@
 import FIREBASE_CONFIG from './firebase.config';
-import { sampleFacultyUser } from './local.config';
-
-/**
- * TODO: Create local.config.ts with content
- *
- * import {AcademicUser} from '@lib/models/user.model';
- *
- * export const sampleFacultyUser: AcademicUser = {
- *   displayName: 'Ricardo Prohaska',
- *   uid: 'RicardoProhaska38@gmail.com',
- *   email: 'richardoProhaska38@nitpy.ac.in',
- *   isFaculty: true,
- * };
- *
- */
 
 export const environment = {
   production: false,
-  localUser: null,
   FIREBASE_CONFIG,
 };
