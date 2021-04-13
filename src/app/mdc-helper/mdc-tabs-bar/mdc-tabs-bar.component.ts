35c55b0b4a1cd5f0d2fea55c02772ac073792674
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-04-13 09:56:17 +0530

    bug fix: @use 'theme' includes styles.scss in every file with this line, mdc-tabs-bar.component.scss is not aware of theme

--- src/app/mdc-helper/mdc-tabs-bar/mdc-tabs-bar.component.ts
+++ src/app/mdc-helper/mdc-tabs-bar/mdc-tabs-bar.component.ts
@@ -1,13 +1,9 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, } from '@angular/core';
 import { MDCTabBar } from '@material/tab-bar'
-import {MDCTab} from '@material/tab'
-import {MDCTabScroller} from '@material/tab-scroller'
-import {MDCTabIndicator} from '@material/tab-indicator'
 
 @Component({
   selector: 'mdc-tabs-bar',
   templateUrl: './mdc-tabs-bar.component.html',
-  styleUrls: ['./mdc-tabs-bar.component.scss'],
   host: {
     class: 'mdc-tabs-bar',
     role: 'tablist'
