dcbc7c5e77e645c21f3706aa0f5324b17acb1fee
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-04-15 21:02:58 +0530

    importUsers cloud function: better error handling

--- firebase/functions/src/importUsers.ts
+++ firebase/functions/src/importUsers.ts
@@ -1,59 +1,63 @@
 import * as admin from 'firebase-admin';
+import {logger} from 'firebase-functions';
+import {INVALID_DATA_ERROR} from './error';
+import {error} from './error.utils';
 import {ImportUsersData} from './models';
 
 const auth = admin.auth();
 const firestore = admin.firestore();
 
 export async function _importUsers(data: ImportUsersData) {
+  // TODO: Check administrative privileges
+  // TODO: Verify Claims
+  // TODO: Check duplicate entries
 
-  try {
+  if (data.users.length > 1000) return error(INVALID_DATA_ERROR, 'cannot import more than 1000 users at a time', data.users.length);
 
-    // TODO: Check administrative privileges
-    // TODO: Verify Claims
-    // TODO: Check duplicate entries
-    if (data.users.length > 1000) return {error: 'MAX_USERS_PER_IMPORT_LIMIT_EXCEEDED'};
+  if (data.claims.find(claim => claim == 'isHod') && !data.dept)
+    return error(INVALID_DATA_ERROR, `'isHod' claim requested without 'dept' field`);
 
-    if (data.claims.find(claim => claim == 'isHod') && !data.dept)
-      return {error: `'isHod' CLAIM REQUESTED WITHOUT 'dept' field`};
+  const claims = data.claims.reduce((obj, claim) => {
+    obj[claim] = claim == 'isHod' ? data.dept : true;
+    return obj;
+  }, {} as any);
 
-    const claims = data.claims.reduce((obj, claim) => {
-      obj[claim] = claim == 'isHod' ? data.dept : true;
-      return obj;
-    }, {} as any);
+  data.users.forEach(user => user.customClaims = claims);
 
-    data.users.forEach(user => user.customClaims = claims);
+  console.log(`Importing ${data.users.length} Users Account Data 🚀`);
 
-    console.log(`Importing ${data.users.length} Users Account Data 🚀`);
-    const response = await auth.importUsers(data.users);
-    console.log(`Importing ${data.users.length} User Accounts Completed 🔥`, 'Response :', response);
+  // creating user accounts
+  const response = await auth.importUsers(data.users);
 
-    const accountsRef = firestore.collection('accounts');
-    const batches = [firestore.batch()];
+  logger.log(`Importing ${data.users.length} Users Accounts Response `,
+    'Success:', response.successCount,
+    'Failed:', response.failureCount
+  );
 
-    if (data.users.length > 500)
-      batches.push(firestore.batch());
+  if (response.failureCount)
+    logger.error(response.errors[0].error);
 
-    data.users.forEach((user, index) => {
-      if (response.errors.some(err => err.index == index)) return; // return if user not created successfully
-      batches[index >= 500 ? 1 : 0].set(accountsRef.doc(user.uid), {
-        name: user.displayName,
-        email: user.email
-      });
+  // uploading to firestore
+  const accountsRef = firestore.collection('accounts');
+  const batches = [firestore.batch()];
+
+  if (data.users.length > 500)
+    batches.push(firestore.batch());
+
+  data.users.forEach((user, index) => {
+    if (response.errors.some(err => err.index == index)) return; // return if user not created successfully
+    batches[index >= 500 ? 1 : 0].set(accountsRef.doc(user.uid), {
+      name: user.displayName,
+      email: user.email
     });
+  });
 
-    console.log('Committing Users To Firestore 🚀');
 
-    await Promise.all(batches.map(batch => batch.commit()
-      .then(_ => console.log(`Committed Users To Firestore 🔥`))
-      .catch(error => console.log(`Failed to Commit Users To Firestore 🙁`, 'Reason :', error))
-    ));
-    console.log('Committing Users Completed 🤚🏻');
+  await Promise.all(batches.map(batch => {
+      return batch.commit().catch(error => console.log(`Failed to Commit Users To Firestore 🙁`, 'Reason :', error));
+    }
+  ));
 
-    return response;
-
-  } catch (e) {
-    console.error(e);
-    return {error: 'internal error'};
-  }
+  return response;
 }
 
