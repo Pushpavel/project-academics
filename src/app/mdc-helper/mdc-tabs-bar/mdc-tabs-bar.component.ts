019c9344efc79477ccc6a5a1cacf87f728aefa96
Pixel <53726418+Pushpavel@users.noreply.github.com>
2021-04-16 21:42:26 +0530

    mdc-tabs-bar background and outline defined

--- src/app/mdc-helper/mdc-tabs-bar/mdc-tabs-bar.component.ts
+++ src/app/mdc-helper/mdc-tabs-bar/mdc-tabs-bar.component.ts
@@ -1,40 +1,40 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, } from '@angular/core';
-import { MDCTabBar } from '@material/tab-bar'
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output,} from '@angular/core';
+import {MDCTabBar} from '@material/tab-bar';
 
 @Component({
   selector: 'mdc-tabs-bar',
   templateUrl: './mdc-tabs-bar.component.html',
   host: {
-    class: 'mdc-tabs-bar',
+    class: 'mdc-tabs-bar mdc-card mdc-card--outlined',
     role: 'tablist'
   }
 })
 export class MdcTabsBarComponent implements OnDestroy, AfterViewInit {
 
-  @Input() tabs: String[] = []
+  @Input() tabs: String[] = [];
 
-  @Output('handletabChange') handletabChange: EventEmitter<any> = new EventEmitter()
+  @Output('handletabChange') handletabChange: EventEmitter<any> = new EventEmitter();
 
-  updateIndex = (e: any) =>  {
-    this.index = e.detail.index
-    this.handletabChange.emit(this.index)
-  }
+  updateIndex = (e: any) => {
+    this.index = e.detail.index;
+    this.handletabChange.emit(this.index);
+  };
 
   index: number = 0;
 
-  private _tabbar?: MDCTabBar
+  private _tabbar?: MDCTabBar;
 
   constructor(private elementRef: ElementRef) {
   }
 
   ngAfterViewInit() {
     this._tabbar = MDCTabBar.attachTo(this.elementRef.nativeElement);
-    this._tabbar.activateTab(this.index)
-    this._tabbar.listen('MDCTabBar:activated', this.updateIndex)
+    this._tabbar.activateTab(this.index);
+    this._tabbar.listen('MDCTabBar:activated', this.updateIndex);
   }
 
   ngOnDestroy() {
-    this._tabbar?.unlisten('MDCTabBar:activated', this.updateIndex)
+    this._tabbar?.unlisten('MDCTabBar:activated', this.updateIndex);
   }
 
 
